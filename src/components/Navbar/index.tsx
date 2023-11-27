4ca17ab481b7933efa433c32d9236f60bccf8530
RajuGangitla <gangitlaraju8520@gmail.com>
2023-11-27 23:08:41 +0530

    dashboard

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,10 +6,11 @@ import { FaBars, FaHome, FaLinkedin, FaQuora } from "react-icons/fa";
 import { FiChevronDown, FiChevronUp, FiLink, FiYoutube } from "react-icons/fi";
 import { AiFillTag, AiOutlinePlusCircle, AiTwotoneHome } from "react-icons/ai";
 import { TbTool } from "react-icons/tb";
-import { BsTicketPerforated } from "react-icons/bs";
+import { BsPersonFillGear, BsTicketPerforated } from "react-icons/bs";
 import { ImFileMusic } from "react-icons/im";
 import useResponsiveIconSize from "../../hooks/useResponsiveIconSize";
 import { UserDataApi } from "../../api/releaseInfo";
+import { IoMdWallet } from "react-icons/io";
 
 
 interface props {
@@ -78,6 +79,16 @@ export default function Index({ isOpen, setIsOpen }: props) {
             ],
         },
         {
+            path: "/Financial",
+            name: "Financial",
+            icon: <IoMdWallet color={'#ffffff'} size={size} />,
+        },
+        {
+            path: "/ManageArtist",
+            name: "Manage Artist",
+            icon: <BsPersonFillGear color={'#ffffff'} size={size} />,
+        },
+        {
             path: "/Label",
             name: "Label",
             icon: <AiFillTag color={'#ffffff'} size={size} />,
@@ -93,6 +104,7 @@ export default function Index({ isOpen, setIsOpen }: props) {
             icon: <FaQuora color={'#ffffff'} size={size} />,
         },
     ];
+    
     return (
         <>
             <div className="bg-neutral-800">
