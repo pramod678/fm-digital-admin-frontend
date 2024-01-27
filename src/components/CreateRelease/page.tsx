b01a8509445f8dedeb8d273a93849b58a1780713
RajuGangitla <gangitlaraju8520@gmail.com>
2024-01-28 00:02:25 +0530

    sunday code

--- src/components/CreateRelease/page.tsx
+++ src/components/CreateRelease/page.tsx
@@ -15,20 +15,20 @@ export default function CreateReleasePage() {
                     {
                         dayOfWeek === 6 && (
                             <>
-                                <p className="text-xl font-semibold mb-2">Out of Office Notice! Please note that our office is closed during the weekends, and as a result.
+                                <p className="text-xl text-center font-semibold mb-2">Out of Office Notice! Please note that our office is closed during the weekends, and as a result.
                                 </p>
-                                <p className="text-xl font-semibold mb-2"> We are unable to distribute releases during this time. Kindly submit your releases on Monday, the beginning of the next work week.</p>
-                                <p className="text-lg font-semibold mb-2">Thank you, Team FM Digital.</p>
+                                <p className="text-xl text-center font-semibold mb-2"> We are unable to distribute releases during this time. Kindly submit your releases on Monday, the beginning of the next work week.</p>
+                                <p className="text-lg text-center font-semibold mb-2">Thank you, Team FM Digital.</p>
                             </>
                         )
                     }
                     {
                         dayOfWeek === 0 && (
                             <>
-                                <p className="text-xl font-semibold mb-2">Good job, You broke the Internet! Just kidding.
-                                    <p className="text-xl font-semibold mb-2">We’re backing up the server at the moment. The site should be back online before monday - 12:00 AM IST.</p>
+                                <p className="text-xl text-center font-semibold mb-2">Good job, You broke the Internet! Just kidding.
+                                    <p className="text-xl text-center font-semibold mb-2">We’re backing up the server at the moment. The site should be back online before monday - 12:00 AM IST.</p>
                                 </p>
-                                <p className="text-lg font-semibold mb-2">Thank you, Team FM Digital.</p>
+                                <p className="text-lg text-center font-semibold mb-2">Thank you, Team FM Digital.</p>
                             </>
                         )
                     }
