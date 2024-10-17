a54e98089354fd688094214c5c3d5ba361a4ab30
pramodcrazibrainsolutions <pramod@crazibrainsolutions.com>
2024-10-17 14:41:26 +0530

    update new

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -2,12 +2,7 @@ import axios from "axios";
 
 const token = localStorage.getItem("token");
 const api = axios.create({
-<<<<<<< HEAD
-    baseURL: "https://api.fmdigitalofficial.com/api/v1" ,
-    // baseURL: "https://fmdigitalofficial.in/api/v1" ,
-=======
     baseURL: "https://api.fmdigitalofficial.com/api/v1",
->>>>>>> 29d79848328f06c1959404d9e3020481403d71fa
     headers: {
         'Access-Control-Allow-Credentials': true,
         "Access-Control-Allow-Origin": "*",
